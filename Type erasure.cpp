bea20b0b42f4952a0af45345ab715324746446c7
7moon120 <92841151+ArtemBystrovOfficial@users.noreply.github.com>
2023-04-20 21:40:53 +0800

    Add files via upload

---
+++ Type erasure.cpp
@@ -0,0 +1,43 @@
+﻿
+// Type erasure side of things
+
+#include <vector>
+#include <iostream>
+
+class any {
+public:
+	template <class T>
+	any(const T& t) : ptr(new TypeErasure<T>(t)) {
+		
+	}
+	template <class T>
+	T cast_any() {
+		if (typeid(T) != ptr->type_info())
+			throw std::exception("bad_cast");
+		return static_cast<TypeErasure<T> *>(ptr.get())->m_t;
+	}
+private:
+
+	class BaseType {
+	public:
+		virtual const std::type_info& type_info() const = 0;
+	};
+
+	template<class T>
+	class TypeErasure : public BaseType {
+	public:
+		TypeErasure(const T & t) : m_t(t){}
+		const std::type_info& type_info() const {
+			return typeid(m_t);
+		}
+		T m_t;
+	};
+
+	std::unique_ptr<BaseType> ptr;
+};
+
+int main() {
+	any test = int(5);
+	auto type = test.cast_any<int>();
+	std::cout << type;
+}
